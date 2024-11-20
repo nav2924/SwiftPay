a9b5462595412aba5b1c8aa7281289c773716ea6
nav2924 <navi29012004@gmail.com>
2024-11-20 21:33:58 +0530

    AuthForm

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AuthForm from "@/components/AuthForm";
 
 const signup = () => {
   return (
-    <div>signup</div>
-  )
-}
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
+};
 
 export default signup
